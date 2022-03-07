b9b6e4ca2ee9759d211e1224ad0d46c299f67f04
Guillermo cala(pc andrea) <guillermo.cala7@gmail.com>
2022-03-07 11:45:44 -0500

    funcion treeview por ingreso manual(ejemplo)

--- viewer.py
+++ viewer.py
@@ -1,8 +1,30 @@
 from tkinter import *
 from tkinter import ttk
 root = Tk()
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+root.title("XML viewer")
+root.geometry("500x500")
+
+arbol = ttk.Treeview(root)
+"""ingreso grados"""
+grado_a = arbol.insert("", END, text="grado_a")
+grado_b = arbol.insert("", END, text="grado_b")
+grado_c = arbol.insert("", END, text="grado_c")
+"""ingreso grado_a"""
+grado_a_estudiante_1 = arbol.insert(grado_a, END, text="estudiante1")
+grado_a_estudiante_2 = arbol.insert(grado_a, END, text="estudiante2")
+grado_a_estudiante_3 = arbol.insert(grado_a, END, text="estudiante3")
+"""ingreso grado_b"""
+grado_b_estudiante_1 = arbol.insert(grado_b, END, text="estudiante1")
+grado_b_estudiante_2 = arbol.insert(grado_b, END, text="estudiante2")
+grado_b_estudiante_3 = arbol.insert(grado_b, END, text="estudiante3")
+"""ingreso grado_c"""
+grado_c_estudiante_1 = arbol.insert(grado_c, END, text="estudiante1")
+grado_c_estudiante_2 = arbol.insert(grado_c, END, text="estudiante2")
+grado_c_estudiante_3 = arbol.insert(grado_c, END, text="estudiante3")
+
+
+arbol.pack()
+
+
+
 root.mainloop()
