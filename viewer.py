2a1629ad6d376e8c7159221722b6b454b9306071
Guillermo cala(pc andrea) <guillermo.cala7@gmail.com>
2022-03-07 13:16:40 -0500

    solo ingresa por funcion nodos debajo de la raiz

--- viewer.py
+++ viewer.py
@@ -1,29 +1,33 @@
 from tkinter import *
 from tkinter import ttk
+
 root = Tk()
 root.title("XML viewer")
 root.geometry("500x500")
 
 arbol = ttk.Treeview(root)
+
+def ingresa_arbol(parent, listaTexto):
+    for item in listaTexto:
+        arbol.insert(parent, END, text=item)
+
+
 """ingreso grados"""
 grado_a = arbol.insert("", END, text="grado_a")
 grado_b = arbol.insert("", END, text="grado_b")
 grado_c = arbol.insert("", END, text="grado_c")
 """ingreso grado_a"""
-grado_a_estudiante_1 = arbol.insert(grado_a, END, text="estudiante1")
-grado_a_estudiante_2 = arbol.insert(grado_a, END, text="estudiante2")
-grado_a_estudiante_3 = arbol.insert(grado_a, END, text="estudiante3")
+grado_a_estudiantes = ["estudiante1", "estudiante2", "estudiante3"]
+ingresa_arbol(grado_a, grado_a_estudiantes)
 """ingreso grado_b"""
-grado_b_estudiante_1 = arbol.insert(grado_b, END, text="estudiante1")
-grado_b_estudiante_2 = arbol.insert(grado_b, END, text="estudiante2")
-grado_b_estudiante_3 = arbol.insert(grado_b, END, text="estudiante3")
+grado_b_estudiantes = ["estudiante1", "estudiante2", "estudiante3"]
+ingresa_arbol(grado_b, grado_a_estudiantes)
 """ingreso grado_c"""
-grado_c_estudiante_1 = arbol.insert(grado_c, END, text="estudiante1")
-grado_c_estudiante_2 = arbol.insert(grado_c, END, text="estudiante2")
-grado_c_estudiante_3 = arbol.insert(grado_c, END, text="estudiante3")
+grado_c_estudiantes = ["estudiante1", "estudiante2", "estudiante3"]
+ingresa_arbol(grado_c, grado_c_estudiantes)
 
 
-arbol.pack()
+arbol.pack(fill = BOTH, expand = True)
 
 
 
